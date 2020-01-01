fe9c2116c31c538fd33e79f6fbbf3026c9708075
y4553r <abougueniyasser@outlook.com>
2020-01-01 20:40:51 +0100

    start tests for GuessedWords

---
+++ src/Jotto/GuessedWords/GuessedWords.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = ({ guessedWords }) => {
+  return (
+    <div data-test='component-guessed-words'>
+      {!guessedWords.length &&
+        <span data-test='guess-instructions'>
+          Try to guess the secret word!
+        </span>}
+    </div>
+  );
+}
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+}
+
+export default GuessedWords;
