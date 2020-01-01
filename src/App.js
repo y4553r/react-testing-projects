e200253be645e3964c032ead2f564d2241016899
y4553r <abougueniyasser@outlook.com>
2020-01-01 18:54:32 +0100

    jotto app first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 import Counter from './Counter/Counter';
+import Jotto from './Jotto/Jotto';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <Jotto />
     </div>
   );
 }
