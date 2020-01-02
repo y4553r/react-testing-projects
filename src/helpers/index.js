00a6d3124df81a52c50736f78d6cda271b071c63
Abougueni Yasser <YABOUGUENI@cpm.co.ma>
2020-01-02 09:46:29 +0100

    set up redux

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,25 +5,25 @@
  * @param {string} secretWord - Secret Word.
  * @returns {number} - Number of letters matched between guessed word and secret word
  */
-export const getLetterMatchCount = (guessedWord, secretWord) => {
-  const freqSecretWordLetters = {};
-  for (let letter of secretWord)
-    freqSecretWordLetters[letter] = ++freqSecretWordLetters[letter] || 1;
-  let count = 0;
-  for (let letter of guessedWord) {
-    if (freqSecretWordLetters[letter]) {
-      delete freqSecretWordLetters[letter]
-      count++;
-    }
-  }
-  return count;
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const guessedWordSet = new Set(guessedWord.split(''));
+  const secretWordSet = new Set(secretWord.split(''));
+  return [...guessedWordSet].filter(letter => secretWordSet.has(letter)).length;
 }
 
-/*
-  another verios of the algorithm
-  function getLetterMatchCount(guessedWord, secretWord) {
-    const guessedWordSet = new Set(guessedWord.split(''));
-    const secretWordSet = new Set(secretWord.split(''));
-    return [...guessedWordSet].filter(letter => secretWordSet.has(letter)).length;
-  }
-*/
+// another verios of the algorithm
+// export const getLetterMatchCount = (guessedWord, secretWord) => {
+//   const freqSecretWordLetters = {};
+//   for (let letter of secretWord)
+//     freqSecretWordLetters[letter] = ++freqSecretWordLetters[letter] || 1;
+//   let count = 0;
+//   for (let letter of guessedWord) {
+//     if (freqSecretWordLetters[letter]) {
+//       delete freqSecretWordLetters[letter]
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {object} state - Success state contains array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {object} - new success state
+ */
+export default (state, action) => {
+  return null;
+}
